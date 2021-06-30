905fa1b4a2b2363397393a03cc4a39eddb78fa6b
Makhmut <grupry@gmail.com>
2021-06-30 19:13:38 +0300

    creating notes with comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import { Edit } from './pages/Edit';
+import { Home } from './pages/Home';
+import { View } from './pages/View';
+import { Navbar } from './components/Navbar';
+import { Alert } from './components/Alert'
+import { AlertState } from './context/alert/AlertState'
+import { FirebaseState } from './context/firebase/FirebaseState';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <div className="container pt-4">
+              <Alert />
+              <Switch>
+                <Route path={'/'} exact component={Home}/>
+                <Route path={'/edit'} component={Edit}/>
+                <Route path={'/view'} component={View}/>
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
   );
 }
 
